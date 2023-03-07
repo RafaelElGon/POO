6c597725516f0f2708e593abcfe48ef5f6a1fb52
RafaelElGon <104838747+RafaelElGon@users.noreply.github.com>
2023-03-07 08:31:14 -0300

    Update TestaSistemaAmigo.java

--- TestaSistemaAmigo.java
+++ TestaSistemaAmigo.java
@@ -1,26 +1,27 @@
-import java.util.ArrayList;
-import java.util.List;
+package SistemaAmigo;
 
 public class TestaSistemaAmigo {
     public static void main(String[] args) {
         SistemaAmigo sys = new SistemaAmigo();
-        sys.cadastrarAmigo("José","josé@email.com");
-        sys.cadastrarAmigo("Maria","maria@email.com");
+        sys.cadastrarAmigo("José", "josé@email.com");
+        sys.cadastrarAmigo("Maria", "maria@email.com");
         try {
             sys.configuraAmigoSecretoDe("josé@email.com", "maria@email.com");
         } catch (AmigoInexistenteException e) {
             System.out.println("Amigo inexistente");
         }
-        Mensagem msg = new MensagemParaAlguem("NEVER GONNA GIVE YOU UP", "josé@email.com", false);
+        Mensagem msg = new MensagemParaAlguem("NEVER GONNA GIVE YOU UP", "maria@email.com", "josé@email.com", false);
         System.out.println(msg.getTextoCompletoAExibir());
+        sys.cadastrarMensagens(msg);
+
+        Mensagem msgTodos = new MensagemParaTodos("Never gonna give you up", "maria@email.com", true);
+        System.out.println(msgTodos.getTextoCompletoAExibir());
+        for (Mensagem m : sys.pesquisaMensagensAnonimas()) {
+            System.out.println(m.getTextoCompletoAExibir());
+        }
+        String secretoPesquisado = sys.pesquisaAmigoSecreto("maria@email.com");
+        if(secretoPesquisado.equals("Amigo Secreto: maria@email.com")){
+            System.out.println("Ok");
+        }
     }
 }
-
-//a) Cadastre dois amigos José e Maria com seus e-mails.
-//        b) Configure o amigo secreto de José como sendo Maria e o amigo secreto de Maria como sendo José. Obs: Lembre
-//        de tratar a exceção que pode ser lançada.
-//        c) Envie uma mensagem anônima de Maria para José.
-//        d) Envie uma mensagem anônima de Maria para todos.
-//        e) Pesquise as mensagens anônimas e imprima o texto completo dessas mensagens através do método
-//        getTextoCompletoAExibir.
-//        f) Pesquise o e-mail do amigo secreto de José e veja se é o e-mail de Maria, imprimindo “Ok” caso seja.
