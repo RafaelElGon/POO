1f1125002869608946b581eeb39d9950c600fb86
RafaelElGon <104838747+RafaelElGon@users.noreply.github.com>
2023-03-07 08:32:34 -0300

    Create TestaSistemaAmigoGUI.java

---
+++ TestaSistemaAmigoGUI.java
@@ -0,0 +1,36 @@
+package SistemaAmigo;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestaSistemaAmigoGUI {
+    public static void main(String[] args) {
+        SistemaAmigo sys = new SistemaAmigo();
+        List<Amigo> amigoSorteio = new ArrayList<>();
+        String msgLimite = JOptionPane.showInputDialog("Informe o máximo de mensagens que o sistema suportará");
+        int qtAmigos = Integer.parseInt(JOptionPane.showInputDialog("Quantos amigos participarão da brincadeira?"));
+
+        for(int i = 0; i <= qtAmigos; i++){
+            String nomeParticipante = JOptionPane.showInputDialog("Nome do participante");
+            String emailParticipante = JOptionPane.showInputDialog("Email do participante");
+            sys.cadastrarAmigo(nomeParticipante, emailParticipante);
+        }
+
+        //usar uma lista diferente pra não interferir com os dados em sys.getAmigos();
+        List<Amigo> amigoList = sys.getAmigos();
+
+        //faz o sorteio e cadastra os sorteados em amigoSorteio
+            for(int i = 0; i <= amigoList.size(); i++){
+            int posicaoSorteada = (int) (Math.random() * sys.getAmigos().size());
+                amigoSorteio.add(amigoList.get(posicaoSorteada));
+                amigoList.remove(amigoList.get(posicaoSorteada));
+                if(i != posicaoSorteada){
+                    sys.configuraAmigoSecretoDe(amigoList.get(i).getEmail(), amigoSorteio.get(i).getEmail());
+            }
+        }
+            for(Amigo a: amigoSorteio){
+                System.out.println(a.getEmailAmigoSorteado());
+            }
+    }
+}
